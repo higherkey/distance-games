530309c8538ac35571327508d124e08f14af23b0
Isaac Ahlfield <isaac.ahlfield@gmail.com>
2022-11-01 15:48:21 -0500

    Completed Building a Board

--- dist/App.f684dadd.js
+++ dist/App.f684dadd.js
@@ -16758,14 +16758,69 @@ function IsDraw(cells) {
 var _client = require("boardgame.io/client");
 var _Game = require("./Game");
 class TicTacToeClient {
-  constructor() {
+  constructor(rootElement) {
     this.client = (0, _client.Client)({
       game: _Game.TicTacToe
     });
     this.client.start();
+    this.rootElement = rootElement;
+    this.createBoard();
+    this.attachListeners();
+    this.client.subscribe(state => this.update(state));
+  }
+  createBoard() {
+    console.log("Creating board...");
+    // Create cells in rows for the Tic Tac Toe board.
+    const rows = [];
+    for (let i = 0; i < 3; i++) {
+      const cells = [];
+      for (let j = 0; j < 3; j++) {
+        const id = 3 * i + j;
+        console.log(`Create board cell ${id}`);
+        cells.push(`<td class='cell' data-id='${id}'></td>`);
+      }
+      rows.push(`<tr>${cells.join("")}</tr>`);
+    }
+
+    // Add the HTML to our app <div>
+    // We'll use the empty <p> to display the game winner later
+    this.rootElement.innerHTML = `
+      <table>${rows.join("")}</table>
+      <p class='winner'></p>`;
+  }
+  attachListeners() {
+    // This event handler will read the cell id from a cell's `data-id` attribute and will make the `clickCell` move
+    const handleCellClick = event => {
+      const id = parseInt(event.target.dataset.id);
+      this.client.moves.clickCell(id);
+    };
+    // Attach the event listener to each of the board cells
+    const cells = this.rootElement.querySelectorAll(".cell");
+    cells.forEach(cell => {
+      cell.onclick = handleCellClick;
+    });
+  }
+  update(state) {
+    // Get all the board cells
+    const cells = this.rootElement.querySelectorAll(".cell");
+    // Update cells to display the values in a game state
+    cells.forEach(cell => {
+      const cellId = parseInt(cell.dataset.id);
+      const cellValue = state.G.cells[cellId];
+      cell.textContent = cellValue !== null ? cellValue : "";
+    });
+    // Get the gameover message element
+    const messageElement = this.rootElement.querySelector(".winner");
+    // Update the element to show a winner if any
+    if (state.ctx.gameover) {
+      messageElement.textContent = state.ctx.gameover.winner !== undefined ? "Winner: " + state.ctx.gameover.winner : "Draw!";
+    } else {
+      messageElement.textContent = "";
+    }
   }
 }
-const app = new TicTacToeClient();
+const appElement = document.getElementById("app");
+const app = new TicTacToeClient(appElement);
 },{"boardgame.io/client":"node_modules/boardgame.io/dist/esm/client.js","./Game":"src/Game.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -16791,7 +16846,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54760" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55145" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
